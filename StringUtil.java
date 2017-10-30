f7f0a7b8718ee19d0cd5290797ec363d321c35aa
小白 <1274464354@qq.com>
2017-10-30 14:49:53 +0800

    add isNullOrEmpty、ignoreCaseReplace

--- StringUtil.java
+++ StringUtil.java
@@ -1,5 +1,8 @@
 package javaUtil1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
 * @模块：[StringUtil]
 * @描述: [字符串处理工具类]
@@ -9,11 +12,9 @@ package javaUtil1;
 */
 
 public class StringUtil {
-
 	/**
 	 * <p>1</p>
-	 * <p>arrayInsertArray</p>
-	 * <p>数组插入到另一个数组特定位置</p>
+	 * <p>方法名称: arrayInsertArray|描述: 数组插入到另一个数组特定位置</p>
 	 * @author ixiaobai
 	 * @param arr 被插入数组
 	 * @param seat 插入位置(>=0；将data插入到arr第seat位置后)
@@ -48,10 +49,45 @@ public class StringUtil {
 		}
 	}
 
+	/**
+	 * <p>2</p>
+	 * <p>方法名称: isNullOrEmpty|描述: 判断字符串是否为null或者空</p>
+	 * @author ixiaobai
+	 * @param source 原字符串
+	 * @return
+	 */
+	public static boolean isNullOrEmpty(String source){
+		if(null == source || "".equals(source)) return true;
+		return false;
+	}
+
+	/**
+	 * <p>3</p>
+	 * <p>方法名称: ignoreCaseReplace|描述: 忽略大小写替换</p>
+	 * @author ixiaobai
+	 * @param source 原字符串
+	 * @param oldString 被替换字符串
+	 * @param newString 替换字符串
+	 * @return
+	 */
+	public static String ignoreCaseReplace(String source, String oldString, String newString){
+		if(StringUtil.isNullOrEmpty(source) || null==oldString || 0==oldString.length()) {
+			return source;
+		}
+		Pattern p = Pattern.compile(oldString, Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(source);
+		String ret=m.replaceAll(newString);
+		return ret;
+	}
+
+
 	public static void main(String[] args) {
 		/**********1、变量池***********/
 		String str1[] = {"1","2","6","7"};
 		String str2[] = {"3","4","5"};
+		String s1 = null;
+		String s2 = null;
+		String s3 = null;
 		/**********2、方法池***********/
 		System.out.println("/**********1***********/");
 		String str3[] = StringUtil.arrayInsertArray(str1, 2, str2);
@@ -64,6 +100,13 @@ public class StringUtil {
 			System.out.print(s+"\t");
 		}
 		System.out.println();
-		System.out.println("/**********1***********/");
+		System.out.println("/**********2***********/");
+		System.out.print(StringUtil.isNullOrEmpty(s1)+"\n");
+		System.out.println("/**********3***********/");
+		s1="a b c d ";
+		s2=" ";
+		s3=",";
+		System.out.print(StringUtil.ignoreCaseReplace(s1, s2, s3)+"\n");
+
 	}
 }
